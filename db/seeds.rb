b897312860b76b61ac38df15cf9e2f2f6d5cb8d4
marcus gray <abundanceisfree@gmail.com>
2016-01-05 17:33:30 -0800

    heroku commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,52 +6,50 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-artists = User.create ([{
-  email: "",
-  password_digest:"",
-  location: "",
+artists = Artist.create ([{
+  location: "Los Angeles",
   name: "Mear1",
-  image_content_type: "http://www.unexplainable.net/brainbox/uploads/2/Mearismmessage.jpg",
+  artsamples: "http://www.unexplainable.net/brainbox/uploads/2/Mearismmessage.jpg",
   description: "High vibrational visionary Urban art",
-  role: 1
 }])
 
-artists = User.create([{
-   email: "",
-  password_digest:"",
-  location: "los Angeles",
-  name: "cryptik",
-  image_content_type: "http://411posters.com/wp-content/uploads/2014/06/cryptik-the-jewel-in-the-lotus-29.5x29_.5-1xrun-04_.jpg",
-  description: "urban zen themed stylized calligraphy",
-  role: 1
-}])
+# artists = User.create([{
+#    email: "",
+#   password_digest:"",
+#   location: "los Angeles",
+#   name: "cryptik",
+#   image_content_type: "http://411posters.com/wp-content/uploads/2014/06/cryptik-the-jewel-in-the-lotus-29.5x29_.5-1xrun-04_.jpg",
+#   description: "urban zen themed stylized calligraphy",
+#   role: 1
+# }])
 
-artists = User.create([{
-   email: "",
-  password_digest:"",
-  location: "los angeles",
-  name: "abcnt",
-  image_content_type: "https://abcnt.files.wordpress.com/2008/03/abcnt.jpg?w=198&h=296",
-  description: "Revolution themed urban activist art",
-  role: 2
-}])
+# artists = User.create([{
+#    email: "",
+#   password_digest:"",
+#   location: "los angeles",
+#   name: "abcnt",
+#   image_content_type: "https://abcnt.files.wordpress.com/2008/03/abcnt.jpg?w=198&h=296",
+#   description: "Revolution themed urban activist art",
+#   role: 2
+# }])
 
-artists = User.create ([{
-   email: "",
-  password_digest:"",
-  name: "the loft exchange",
-  image_content_type: "https://trademarks.justia.com/media/image.php?serial=77380173",
-  location: "DTLA",
-  role: 2
-  }])
+# artists = User.create ([{
+#    email: "",
+#   password_digest:"",
+#   name: "the loft exchange",
+#   image_content_type: "https://trademarks.justia.com/media/image.php?serial=77380173",
+#   location: "DTLA",
+#   role: 2
+#   }])
 
 
 
-artists = User.create ([{
-   email: "",
-  password_digest:"",
-  name: "DTLA_Lofts",
-  image_content_type: "http://pauljohnston.com/wp-content/files_mf/006_436_wellington_w_603.jpg",
-  location: "DTLA",
-  role: 2
-  }])
+# artists = User.create ([{
+#    email: "",
+#   password_digest:"",
+#   name: "DTLA_Lofts",
+#   image_content_type: "http://pauljohnston.com/wp-content/files_mf/006_436_wellington_w_603.jpg",
+#   location: "DTLA",
+#   role: 2
+#   }])
+
