6ca1d40c34100a9399469e6853346a5d0fdf485b
marcus gray <abundanceisfree@gmail.com>
2016-01-07 09:02:36 -0800

    added client page

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,22 @@ get 'users/index' => 'users#index'
 get 'users/new' => 'users#new'
 
 post 'user/' => 'users#create'
-
+get 'user/edit' => 'users#edit'
 get 'users/:id' => 'users#show', as: :user_show
 
 get 'signup'  => 'users#new'
+
+
 get 'sessions/new' => 'sessions#new',as: :new_session
 
 post 'sessions/new' => 'sessions#create', as: :create_session
-delete 'sessions/destroy' => 'sessions#destroy'
+
+get 'client/new' => 'client#new'
+
+get 'artist/new' => 'artist#new'
+
+get 'profile_type/new' => 'profile_type#new'
+
 
 
 get    'login'   => 'sessions#new'
