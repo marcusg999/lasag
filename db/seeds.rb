66f0c4f9761911a0e4bb75ccd64b926bbea10e36
marcus gray <abundanceisfree@gmail.com>
2016-01-07 23:35:11 -0800

    attempting new webfont

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,43 +13,43 @@ artists = Artist.create ([{
   description: "High vibrational visionary Urban art",
 }])
 
-artists = Artist.create([{
-   email: "",
-  password_digest:"",
-  location: "los Angeles",
-  name: "cryptik",
-  artsamples: "http://411posters.com/wp-content/uploads/2014/06/cryptik-the-jewel-in-the-lotus-29.5x29_.5-1xrun-04_.jpg",
-  description: "urban zen themed stylized calligraphy",
+# artists = Artist.create([{
+#    email: "",
+#   password_digest:"",
+#   location: "los Angeles",
+#   name: "cryptik",
+#   artsamples: "http://411posters.com/wp-content/uploads/2014/06/cryptik-the-jewel-in-the-lotus-29.5x29_.5-1xrun-04_.jpg",
+#   description: "urban zen themed stylized calligraphy",
 
-}])
+# }])
 
-artists = Artist.create([{
-   email: "",
-  password_digest:"",
-  location: "los angeles",
-  name: "abcnt",
-  artsamples: "https://abcnt.files.wordpress.com/2008/03/abcnt.jpg?w=198&h=296",
-  description: "Revolution themed urban activist art",
+# artists = Artist.create([{
+#    email: "",
+#   password_digest:"",
+#   location: "los angeles",
+#   name: "abcnt",
+#   artsamples: "https://abcnt.files.wordpress.com/2008/03/abcnt.jpg?w=198&h=296",
+#   description: "Revolution themed urban activist art",
 
-}])
+# }])
 
-client = Client.create ([{
-   email: "",
-  password_digest:"",
-  name: "the loft exchange",
-  image_content_type: "https://trademarks.justia.com/media/image.php?serial=77380173",
-  location: "DTLA",
+# client = Client.create ([{
+#    email: "",
+#   password_digest:"",
+#   name: "the loft exchange",
+#   image_content_type: "https://trademarks.justia.com/media/image.php?serial=77380173",
+#   location: "DTLA",
 
-  }])
+#   }])
 
 
 
-client = Client.create ([{
-   email: "",
-  password_digest:"",
-  name: "DTLA_Lofts",
-  image_content_type: "http://pauljohnston.com/wp-content/files_mf/006_436_wellington_w_603.jpg",
-  location: "DTLA",
+# client = Client.create ([{
+#    email: "",
+#   password_digest:"",
+#   name: "DTLA_Lofts",
+#   image_content_type: "http://pauljohnston.com/wp-content/files_mf/006_436_wellington_w_603.jpg",
+#   location: "DTLA",
 
-  }])
+#   }])
 
